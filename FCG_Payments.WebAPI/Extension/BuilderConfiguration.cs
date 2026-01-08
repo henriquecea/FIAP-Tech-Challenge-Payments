ec1757235bcb4591ff6b6cbc78a27b319af18cc5
Henrique Cea <henrique.cea@peakinvest.com.br>
2026-01-07 21:27:34 -0300

    fix swagger

--- FCG_Payments.WebAPI/Extension/BuilderConfiguration.cs
+++ FCG_Payments.WebAPI/Extension/BuilderConfiguration.cs
@@ -51,7 +51,7 @@ public static class BuilderConfiguration
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen(c =>
         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Fiap Tech Challenge", Version = "v1" });
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Fiap Tech Challenge - Payments", Version = "v1" });
             c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
             {
                 In = ParameterLocation.Header,
--- FCG_Payments.WebAPI/Program.cs
+++ FCG_Payments.WebAPI/Program.cs
@@ -34,7 +34,7 @@ using (var scope = app.Services.CreateScope())
 }
 
 app.UseSwagger();
-app.UseSwagger();
+app.UseSwaggerUI();
 
 // Redirecionamento da raiz para /swagger
 app.MapGet("/", context =>
